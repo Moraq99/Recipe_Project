1100805e813d230f15108922f8c0fe513e7592fc
kimbi91 <subscribefan@gmail.com>
2022-09-29 17:42:00 +0200

    IO exeption import removed from main controller

--- src/main/java/com/example/recipe_project/controller/MainController.java
+++ src/main/java/com/example/recipe_project/controller/MainController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
